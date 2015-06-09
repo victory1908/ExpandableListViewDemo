7efd52a297d4f16992e664ee84f49fb44046ceec
Anand Bose <anandbose@Anands-MacBook-Air.local>
2015-06-09 17:06:27 +0530

    Initial commit

---
+++ app/src/main/java/com/anandbose/expandablelistdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.anandbose.expandablelistdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        List<ExpandableListAdapter.Item> data = new ArrayList<>();
+
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "Fruits"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Apple"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Orange"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Banana"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "Cars"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Audi"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Aston Martin"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "BMW"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Cadillac"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "Places"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Kerala"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Tamil Nadu"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Karnataka"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Maharashtra"));
+
+        recyclerview.setAdapter(new ExpandableListAdapter(data));
+    }
+}
